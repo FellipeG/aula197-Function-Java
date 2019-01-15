0a475ec74299cf7705c117825eabb9708b854de1
FellipeG <fellipegarciasrj@yahoo.com.br>
2019-01-15 21:11:56 -0200

    Inline lambda expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import model.entities.Product;
@@ -21,10 +20,8 @@ public class Program {
 		list.add(new Product("Mouse", 50.0));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-		
-		Function<Product, String> function = p -> p.getName().toUpperCase();
 
-		List<String> names = list.stream().map(function).collect(Collectors.toList());
+		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 		
